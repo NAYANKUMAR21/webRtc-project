2a127fe73d999718ec10d4c88305bc6166a7efa6
Nayan Kumar <naayaankumar@gmail.com>
2025-01-04 09:43:21 +0530

    invalid syntax error solve

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { SocketProvider } from './Context/Socket.provider.jsx';
+import { SocketProvider } from './context/Socket.provider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
